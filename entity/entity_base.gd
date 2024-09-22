18e56f2660f6cea53ff3e923082b0368922349b9
Sean Carlyle <808jalapeno@gmail.com>
2024-09-22 15:52:23 -0700

    adding set/get clamp for hp variables and death signal

--- entity/entity_base.gd
+++ entity/entity_base.gd
@@ -1,7 +1,26 @@
 extends CharacterBody2D
 
-@export var hp_max: int = 100
-@export var hp: int = hp_max
+signal hp_changed(new_hp)
+signal hp_max_changed(new_hp_max)
+signal died
+
+@export var hp_max: int = 100:
+	set(value):
+		if value != hp_max:
+			hp_max = max(0, value)
+			emit_signal("hp_max_changed", hp_max)
+			self.hp = hp
+
+@export var hp: int = hp_max:
+	set(value):
+		if value != hp:
+			hp = clamp(value, 0, hp_max)
+			emit_signal("hp_changed", hp)
+			if hp == 0:
+				emit_signal("died")
+	get:
+		return hp
+		
 @export var defense: int = 0
 
 @export var SPEED: int = 75
@@ -9,7 +28,6 @@ extends CharacterBody2D
 @onready var sprite = $Sprite2D
 @onready var collShape = $CollisionShape2D
 @onready var hurtbox = $hurtbox
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -35,3 +53,7 @@ func recieve_damage(base_damage: int):
 	
 func _on_hurtbox_area_entered(hitbox):
 	recieve_damage(hitbox.damage)
+
+
+func _on_died():
+	death()
