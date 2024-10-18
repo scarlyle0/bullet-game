d73df497683b77aa1dad1a0b8f5073a29d9f3ee2
Sean Carlyle <808jalapeno@gmail.com>
2024-10-17 22:53:38 -0700

    adding menu

---
+++ menu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://main.tscn")
+
+
+func _on_options_pressed():
+	get_tree().change_scene_to_file("res://levels/options.tscn")
+
+
+func _on_quit_pressed():
+	get_tree().quit()
